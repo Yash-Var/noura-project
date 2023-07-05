dd4838ca0f28768e54d0362f0b8a9c07d1f7b0e1
IshanBh7 <113094607+IshanBh7@users.noreply.github.com>
2023-07-06 01:23:02 +0530

    Add files via upload
    Breathe-page added

---
+++ src/Breathe.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import './Breathe.css';
+import Breathe2 from './Breathe2';
+
+const Breathe = () => {
+  const [startClicked, setStartClicked] = useState(false);
+  const [sessionDuration, setSessionDuration] = useState(0);
+
+  const handleStartClick = () => {
+    setStartClicked(true);
+  };
+
+  const handleDurationChange = (e) => {
+    const value = parseInt(e.target.value);
+    if (!isNaN(value)) {
+      setSessionDuration(Math.max(0, Math.min(60, value)));
+    }
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'ArrowUp') {
+      e.preventDefault();
+      setSessionDuration((prevDuration) => Math.min(60, prevDuration + 1));
+    } else if (e.key === 'ArrowDown') {
+      e.preventDefault();
+      setSessionDuration((prevDuration) => Math.max(0, prevDuration - 1));
+    }
+  };
+
+  return (
+    <div className="breath">
+      {!startClicked ? (
+        <div className="frame" onClick={handleStartClick}>
+          <div className="Start">Start</div>
+        </div>
+      ) : (
+        <Breathe2 />
+      )}
+      <p className="p">How long do you want this session to last?</p>
+      <div className="frame-2">
+        <div className="div-wrapper">
+          <input
+            type="number"
+            className="text-wrapper-2"
+            min="0"
+            max="60"
+            value={sessionDuration}
+            onChange={handleDurationChange}
+            onKeyDown={handleKeyDown}
+          />
+        </div>
+        <div className="text-wrapper-3">minutes</div>
+      </div>
+      <div className="frame-3">
+        <div className="text-wrapper-4">Noura</div>
+        <img className="cancel" alt="Cancel" src="cancel.png" />
+      </div>
+    </div>
+  );
+};
+
+export default Breathe;
