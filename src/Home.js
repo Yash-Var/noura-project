e209d6bcd255940a2834938e136129b522a537f6
Jay <69604238+MaybeJaybe@users.noreply.github.com>
2023-07-05 12:55:30 -0700

    fixed some styling

--- src/Home.js
+++ src/Home.js
@@ -4,25 +4,27 @@ import './Home.css';
 function Home() {
 	return (
 		<div className='container'>
-			<nav>
-				<div className='title'>Noura</div></nav>
+			<nav className='nav'>
+				<div className='title'>Noura</div>
+				<button className='close'>x</button>
+			</nav>
 			<div className='button-select'>
 				<p className='prompt'>What would you like to do?</p>
-				<div className='grid-container'>
-					<div className='grid-item'>
-						<button type='button' id='b1'>Stretch</button>
+				<div className='btn-container'>
+					<div className='btn-item'>
+						<button type='button' id='b1' className='button'>Stretch</button>
 					</div>
-					<div className='grid-item'>
-						<button type='button' id='b2'>Focus</button>
+					<div className='btn-item'>
+						<button type='button' id='b2' className='button'>Focus</button>
 					</div>
-					<div className='grid-item'>
-						<button type='button' id='b3'>Breathe</button>
+					<div className='btn-item'>
+						<button type='button' id='b3' className='button'>Breathe</button>
 					</div>
-					<div className='grid-item'>
-						<button type='button' id='b4'>Water</button>
+					<div className='btn-item'>
+						<button type='button' id='b4' className='button'>Water</button>
 					</div>
-					<div className='grid-item'>
-						<button type='button' id='b5'>Custom</button>
+					<div className='btn-item'>
+						<button type='button' id='b5' className='button'>Custom</button>
 					</div>
 				</div>
 			</div>
